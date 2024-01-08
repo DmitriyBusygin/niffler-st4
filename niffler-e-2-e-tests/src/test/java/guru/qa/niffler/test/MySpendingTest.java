3905323635330fc0eaa787082b1699dffc50ebd9
DmitriyBusygin <busdmv@gmail.com>
2024-01-08 20:27:33 +0300

    File conflict during synchronization

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/MySpendingTest.java
@@ -0,0 +1,47 @@
+package guru.qa.niffler.test;
+
+import com.codeborne.selenide.Configuration;
+import guru.qa.niffler.jupiter.GenerateCategory;
+import guru.qa.niffler.jupiter.GenerateSpend;
+import guru.qa.niffler.model.CurrencyValues;
+import guru.qa.niffler.model.SpendJson;
+import guru.qa.niffler.page.LoginPage;
+import guru.qa.niffler.page.MainPage;
+import guru.qa.niffler.page.WelcomePage;
+import org.junit.jupiter.api.Test;
+
+public class MySpendingTest {
+    static {
+        Configuration.browserSize = "1980x1024";
+    }
+
+    private final WelcomePage welcomePage = new WelcomePage();
+    private final LoginPage loginPage = new LoginPage();
+    private final MainPage mainPage = new MainPage();
+
+    @GenerateCategory(
+            username = "duck",
+            category = "Обучение")
+    @GenerateSpend(
+            username = "duck",
+            description = "QA.GURU Advanced 4",
+            amount = 72500.00,
+            category = "Обучение",
+            currency = CurrencyValues.RUB
+    )
+    @Test
+    void spendingShouldBeDeletedByButtonDeleteSpending(SpendJson spend) {
+        welcomePage.openPage()
+                .clickLogin();
+
+        loginPage.loadPage()
+                .setUserName(spend.username())
+                .setPassword("12345")
+                .clickSignIn();
+
+        mainPage.loadPage()
+                .selectSpendByDescription(spend.description())
+                .clickDeleteSelected()
+                .checkCountRowsInSpendingTable(0);
+    }
+}
